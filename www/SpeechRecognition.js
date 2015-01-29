ec17fa0a6be0888355bd6b8476dcef90a5858328
AnGe724 <edmondan724@gmail.com>
2015-01-29 22:48:57 +0800

    Fixed JS

--- www/SpeechRecognition.js
+++ www/SpeechRecognition.js
@@ -1,81 +1,98 @@
-var exec = require("cordova/exec");
+cordova.define("org.apache.cordova.speech.speechrecognition.SpeechRecognition", function(require, exports, module) {
+    
+    cordova.define("cordova/plugin/SpeechRecognition", function(require, exports, module) {
+               
+        var exec = require("cordova/exec");
 
 /** 
-    attribute SpeechGrammarList grammars;
-    attribute DOMString lang;
-    attribute boolean continuous;
-    attribute boolean interimResults;
-    attribute unsigned long maxAlternatives;
-    attribute DOMString serviceURI;
- */
-var SpeechRecognition = function () {
-    this.grammars = null;
-    this.lang = "en";
-    this.continuous = false;
-    this.interimResults = false;
-    this.maxAlternatives = 1;
-    this.serviceURI = "";
-    
-    // event methods
-    this.onaudiostart = null;
-    this.onsoundstart = null;
-    this.onspeechstart = null;
-    this.onspeechend = null;
-    this.onsoundend = null;
-    this.onaudioend = null;
-    this.onresult = null;
-    this.onnomatch = null;
-    this.onerror = null;
-    this.onstart = null;
-    this.onend = null;
+        attribute SpeechGrammarList grammars;
+        attribute DOMString lang;
+        attribute boolean continuous;
+        attribute boolean interimResults;
+        attribute unsigned long maxAlternatives;
+        attribute DOMString serviceURI;
+*/
+        var SpeechRecognition = function () {
+            this.grammars = null;
+            this.lang = "en";
+            this.continuous = false;
+            this.interimResults = false;
+            this.maxAlternatives = 1;
+            this.serviceURI = "";
+            
+            // event methods
+            this.onaudiostart = null;
+            this.onsoundstart = null;
+            this.onspeechstart = null;
+            this.onspeechend = null;
+            this.onsoundend = null;
+            this.onaudioend = null;
+            this.onresult = null;
+            this.onnomatch = null;
+            this.onerror = null;
+            this.onstart = null;
+            this.onend = null;
 
-    exec(function() {
-        console.log("initialized");
-    }, function(e) {
-        console.log("error: " + e);
-    }, "SpeechRecognition", "init", []);
-};
+            exec(function() {
+                console.log("initialized");
+            }, function(e) {
+                console.log("error: " + e);
+            }, "SpeechRecognition", "init", []);
+        };
 
-SpeechRecognition.prototype.start = function() {
-    var that = this;
-    var successCallback = function(event) {
-        if (event.type === "audiostart" && typeof that.onaudiostart === "function") {
-            that.onaudiostart(event);
-        } else if (event.type === "soundstart" && typeof that.onsoundstart === "function") {
-            that.onsoundstart(event);
-        } else if (event.type === "speechstart" && typeof that.onspeechstart === "function") {
-            that.onspeechstart(event);
-        } else if (event.type === "speechend" && typeof that.onspeechend === "function") {
-            that.onspeechend(event);
-        } else if (event.type === "soundend" && typeof that.onsoundend === "function") {
-            that.onsoundend(event);
-        } else if (event.type === "audioend" && typeof that.onaudioend === "function") {
-            that.onaudioend(event);
-        } else if (event.type === "result" && typeof that.onresult === "function") {
-            that.onresult(event);
-        } else if (event.type === "nomatch" && typeof that.onnomatch === "function") {
-            that.onnomatch(event);
-        } else if (event.type === "start" && typeof that.onstart === "function") {
-            that.onstart(event);
-        } else if (event.type === "end" && typeof that.onend === "function") {
-            that.onend(event);
-        }
-    };
-    var errorCallback = function(err) {
-        if (typeof that.onerror === "function") {
-            that.onerror(err);
-        }
-    };
+        SpeechRecognition.prototype.init = function(key) {
+            exec(null, null, "SpeechRecognition", "init", [key]);
+        };
+               
+        SpeechRecognition.prototype.start = function() {
+            var that = this;
+            var successCallback = function(event) {
+                if (event.type === "audiostart" && typeof that.onaudiostart === "function") {
+                    that.onaudiostart(event);
+                } else if (event.type === "soundstart" && typeof that.onsoundstart === "function") {
+                    that.onsoundstart(event);
+                } else if (event.type === "speechstart" && typeof that.onspeechstart === "function") {
+                    that.onspeechstart(event);
+                } else if (event.type === "speechend" && typeof that.onspeechend === "function") {
+                    that.onspeechend(event);
+                } else if (event.type === "soundend" && typeof that.onsoundend === "function") {
+                    that.onsoundend(event);
+                } else if (event.type === "audioend" && typeof that.onaudioend === "function") {
+                    that.onaudioend(event);
+                } else if (event.type === "result" && typeof that.onresult === "function") {
+                    that.onresult(event);
+                } else if (event.type === "nomatch" && typeof that.onnomatch === "function") {
+                    that.onnomatch(event);
+                } else if (event.type === "start" && typeof that.onstart === "function") {
+                    that.onstart(event);
+                } else if (event.type === "end" && typeof that.onend === "function") {
+                    that.onend(event);
+                }
+            };
+            var errorCallback = function(err) {
+                if (typeof that.onerror === "function") {
+                    that.onerror(err);
+                }
+            };
 
-    exec(successCallback, errorCallback, "SpeechRecognition", "start", [this.lang]);
-};
+            exec(successCallback, errorCallback, "SpeechRecognition", "start", [this.lang]);
+        };
 
-SpeechRecognition.prototype.stop = function() {
-    exec(null, null, "SpeechRecognition", "stop", []);
-};
+        SpeechRecognition.prototype.stop = function() {
+            exec(null, null, "SpeechRecognition", "stop", []);
+        };
 
-SpeechRecognition.prototype.abort = function() {
-    exec(null, null, "SpeechRecognition", "abort", []);
-};
+        SpeechRecognition.prototype.abort = function() {
+            exec(null, null, "SpeechRecognition", "abort", []);
+        };
 
-module.exports = SpeechRecognition;
+                   
+        var myplugin = new SpeechRecognition();
+
+        module.exports = myplugin;
+    });
+        
+    var SpeechRecognition = cordova.require("cordova/plugin/SpeechRecognition");
+    module.exports = SpeechRecognition;
+
+});
